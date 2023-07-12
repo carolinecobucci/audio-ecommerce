2ecf2fa49d9a4e7c06342c4652e5f740aa33b9fa
Carol Cobucci <100380628+cobuti@users.noreply.github.com>
2023-07-12 08:58:28 -0300

    created product list item component

---
+++ src/components/ProductListItem/ProductListItem.tsx
@@ -0,0 +1,25 @@
+import styles from "./ProductListItem.module.css";
+
+const ProductListItem = () => {
+  return (
+    <div className={styles.productGridContainer}>
+      <div className={styles.imgBg}>
+        <img src="/src/assets/headphone.png" alt="product" />
+      </div>
+      <div className={styles.productInfo}>
+        <div className={styles.productNamePrice}>
+          <h1 className={styles.productName}>TMA-2 HD Wireless</h1>
+          <h2 className={styles.productPrice}>USD 350</h2>
+        </div>
+        <div className={styles.reviewContainer}>
+          <img className={styles.starImg} src="/src/assets/star-fill-icon.svg" alt="star icon" />
+          <p className={styles.rate}>4.6</p>
+          <p className={styles.reviewNumber}> 86 Reviews</p>
+          <img src="/src/assets/more-vertical-icon.svg" alt="see more icon" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductListItem;
