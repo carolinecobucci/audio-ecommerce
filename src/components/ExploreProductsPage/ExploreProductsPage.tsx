ed78502f91b040a689880a2787e5640325b85454
Carol Cobucci <100380628+cobuti@users.noreply.github.com>
2023-07-12 09:31:39 -0300

    created explore products page

---
+++ src/components/ExploreProductsPage/ExploreProductsPage.tsx
@@ -0,0 +1,26 @@
+import ProductGrid from "../ProductGrid/ProductGrid";
+import styles from "./ExploreProductsPage.module.css";
+
+const ExploreProductsPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.navBar}>
+        <img src="/src/assets/chevron-left-icon.svg" alt="back" />
+        <img src="/src/assets/shopping-cart-icon.svg" alt="shopping cart" />
+      </div>
+      <div className={styles.titleContainer}>
+        <p className={styles.featureProducts}>Feature Products</p>
+        <h2 className={styles.seeAll}>See all products</h2>
+      </div>
+      <div className={styles.filter}>
+        <img className={styles.filterImg} src="/src/assets/sliders-icon.svg" alt="filter" />
+        <p className={styles.filterText}>Filter</p>
+      </div>
+      <div className={styles.productsBg}>
+        <ProductGrid />
+      </div>
+    </div>
+  );
+};
+
+export default ExploreProductsPage;
