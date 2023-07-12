7986fb40e9764fd892b8d33015ed308ec0c21698
Carol Cobucci <100380628+cobuti@users.noreply.github.com>
2023-07-12 19:18:05 -0300

    created review list component

---
+++ src/components/ReviewList/ReviewList.tsx
@@ -0,0 +1,154 @@
+import styles from "./ReviewList.module.css";
+
+const ReviewList = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.userProfile}>
+        <img
+          className={styles.userProfileImg}
+          src="/src/assets/user-profile-picture.svg"
+          alt="user profile picture"
+        />
+        <div className={styles.userNameRate}>
+          <p className={styles.userName}>Madelina</p>
+          <p className={styles.userRate}>
+            <div className={styles.starsContainer}>
+              <img
+                className={styles.starReview}
+                src="/src/assets/star-filled-icon.svg"
+                alt="stars"
+              />
+              <img
+                className={styles.starReview}
+                src="/src/assets/star-filled-icon.svg"
+                alt="stars"
+              />
+              <img
+                className={styles.starReview}
+                src="/src/assets/star-filled-icon.svg"
+                alt="stars"
+              />
+              <img
+                className={styles.starReview}
+                src="/src/assets/star-filled-icon.svg"
+                alt="stars"
+              />
+              <img
+                className={styles.starReview}
+                src="/src/assets/star-outlined-icon.svg"
+                alt="stars"
+              />
+            </div>
+          </p>
+        </div>
+      </div>
+      <p className={styles.reviewText}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua.
+      </p>
+      <div className={styles.userProfile}>
+        <img
+          className={styles.userProfileImg}
+          src="/src/assets/user-profile-picture.svg"
+          alt="user profile picture"
+        />
+        <div className={styles.userNameRate}>
+          <p className={styles.userName}>Irfan</p>
+          <p className={styles.userRate}>
+            <div className={styles.starsContainer}>
+              <img
+                className={styles.starReview}
+                src="/src/assets/star-filled-icon.svg"
+                alt="stars"
+              />
+              <img
+                className={styles.starReview}
+                src="/src/assets/star-filled-icon.svg"
+                alt="stars"
+              />
+              <img
+                className={styles.starReview}
+                src="/src/assets/star-filled-icon.svg"
+                alt="stars"
+              />
+              <img
+                className={styles.starReview}
+                src="/src/assets/star-filled-icon.svg"
+                alt="stars"
+              />
+              <img
+                className={styles.starReview}
+                src="/src/assets/star-filled-icon.svg"
+                alt="stars"
+              />
+            </div>
+          </p>
+        </div>
+      </div>
+      <p className={styles.reviewText}>
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur.
+        <div className={styles.reviewImg}>
+          <img
+            className={styles.reviewImg}
+            src="/src/assets/headphone-review.png"
+            alt="headphone"
+          />
+          <img
+            className={styles.reviewImg}
+            src="/src/assets/headphone-review.png"
+            alt="headphone"
+          />
+          <img
+            className={styles.reviewImg}
+            src="/src/assets/headphone-review.png"
+            alt="headphone"
+          />
+        </div>
+      </p>
+
+      <div className={styles.userProfile}>
+        <img
+          className={styles.userProfileImg}
+          src="/src/assets/user-profile-picture.svg"
+          alt="user profile picture"
+        />
+        <div className={styles.userNameRate}>
+          <p className={styles.userName}>Ravi Putra</p>
+          <p className={styles.userRate}>
+            <div className={styles.starsContainer}>
+              <img
+                className={styles.starReview}
+                src="/src/assets/star-filled-icon.svg"
+                alt="stars"
+              />
+              <img
+                className={styles.starReview}
+                src="/src/assets/star-filled-icon.svg"
+                alt="stars"
+              />
+              <img
+                className={styles.starReview}
+                src="/src/assets/star-filled-icon.svg"
+                alt="stars"
+              />
+              <img
+                className={styles.starReview}
+                src="/src/assets/star-filled-icon.svg"
+                alt="stars"
+              />
+              <img
+                className={styles.starReview}
+                src="/src/assets/star-filled-icon.svg"
+                alt="stars"
+              />
+            </div>
+          </p>
+        </div>
+      </div>
+      <p className={styles.reviewText}>Excepteur sint occaecat cupidatat non proident</p>
+    </div>
+  );
+};
+
+export default ReviewList;
