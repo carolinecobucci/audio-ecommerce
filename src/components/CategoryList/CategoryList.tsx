abcea6b8be049b34ec1a6ec30f7943a85c19ae4f
Carol Cobucci <100380628+cobuti@users.noreply.github.com>
2023-07-12 19:16:52 -0300

    added category list component

--- src/components/CategoryList/CategoryList.tsx
+++ src/components/CategoryList/CategoryList.tsx
@@ -3,10 +3,25 @@ import styles from "./CategoryList.module.css";
 const CategoryList = () => {
   return (
     <div className={styles.categoryContainer}>
-      <div className={styles.category}>Headphone</div>
-      <div className={styles.category}>Headband</div>
-      <div className={styles.category}>Earpads</div>
-      <div className={styles.category}>Cable</div>
+      <input className={styles.radio} type="radio" name="category" id="headphone" />
+      <label className={styles.category} htmlFor="headphone">
+        Headphone
+      </label>
+
+      <input className={styles.radio} type="radio" name="category" id="headband" />
+      <label className={styles.category} htmlFor="headband">
+        Headband
+      </label>
+
+      <input className={styles.radio} type="radio" name="category" id="earpads" />
+      <label className={styles.category} htmlFor="earpads">
+        Earpads
+      </label>
+
+      <input className={styles.radio} type="radio" name="category" id="cable" />
+      <label className={styles.category} htmlFor="cable">
+        Cable
+      </label>
     </div>
   );
 };
