5c1a9d31b3e46d15aba4c9295ab96a3cba61fea5
Carol Cobucci <100380628+cobuti@users.noreply.github.com>
2023-07-16 14:49:13 -0300

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,41 @@ import ProductOverview from "./components/ProductOverview/ProductOverview";
 import SearchPage from "./components/SearchPage/SearchPage";
 import SignInPage from "./components/SignInPage/SignInPage";
 import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
+import { Route, Routes, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <SignInPage />
+      <Routes>
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/explore-products" element={<ExploreProductsPage />} />
+        <Route path="/product-overview" element={<ProductOverview />} />
+        <Route path="/shopping-cart" element={<ShoppingCart />} />
+      </Routes>
+      {/* <Filter /> */}
 
-      <HomePage />
-      <SearchPage />
-      <ExploreProductsPage />
-      <Filter />
-      <ProductOverview />
-      <ShoppingCart />
+      <ul>
+        <li>
+          <Link to="/sign-in">Sign In</Link>
+        </li>
+        <li>
+          <Link to="/">Home Page</Link>
+        </li>
+        <li>
+          <Link to="/search">Search Page</Link>
+        </li>
+        <li>
+          <Link to="/explore-products">Explore Products</Link>
+        </li>
+        <li>
+          <Link to="/product-overview">Product Overview</Link>
+        </li>
+        <li>
+          <Link to="/shopping-cart">Shopping Cart</Link>
+        </li>
+      </ul>
     </>
   );
 }
