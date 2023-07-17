62701b7d13d1d5f85cc59511a7b260eff6e62acb
Carol Cobucci <100380628+cobuti@users.noreply.github.com>
2023-07-16 21:57:08 -0300

    css small fixes

--- src/components/ProductGrid/ProductGrid.tsx
+++ src/components/ProductGrid/ProductGrid.tsx
@@ -16,8 +16,10 @@ const ProductGrid = ({ productName, productPrice, rate, reviewNumber }: productP
         <h2 className={styles.productPrice}>{productPrice}</h2>
       </div>
       <div className={styles.reviewContainer}>
-        <img src="/src/assets/star-fill-icon.svg" alt="star icon" />
-        <p className={styles.rate}>{rate}</p>
+        <div className={styles.starReviews}>
+          <img src="/src/assets/star-fill-icon.svg" alt="star icon" />
+          <p className={styles.rate}>{rate}</p>
+        </div>
         <p className={styles.reviewNumber}>{reviewNumber}</p>
         <img src="/src/assets/more-vertical-icon.svg" alt="see more icon" />
       </div>
--- src/components/ProductListItem/ProductListItem.tsx
+++ src/components/ProductListItem/ProductListItem.tsx
@@ -12,8 +12,10 @@ const ProductListItem = () => {
           <h2 className={styles.productPrice}>USD 350</h2>
         </div>
         <div className={styles.reviewContainer}>
-          <img className={styles.starImg} src="/src/assets/star-fill-icon.svg" alt="star icon" />
-          <p className={styles.rate}>4.6</p>
+          <div className={styles.starReviews}>
+            <img className={styles.starImg} src="/src/assets/star-fill-icon.svg" alt="star icon" />
+            <p className={styles.rate}>4.6</p>
+          </div>
           <p className={styles.reviewNumber}> 86 Reviews</p>
           <img src="/src/assets/more-vertical-icon.svg" alt="see more icon" />
         </div>
