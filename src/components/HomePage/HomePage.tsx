557714e08b76b72e55087e38978e34a92afcb7a8
Carol Cobucci <100380628+cobuti@users.noreply.github.com>
2023-07-19 18:55:24 -0300

    added user profile icon

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -43,7 +43,7 @@ const HomePage = () => {
           {photoURL ? (
             <img src={photoURL} alt="profile picture" />
           ) : (
-            <span>No profile picture available</span>
+            <img src="/src/assets/user.png" alt="profile picture" />
           )}
         </button>
       </div>
